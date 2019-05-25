2f51e0cb7037537de759fce494fbb646a880464b
lord4800 <killerontheroad4800@gmail.com>
2019-05-25 18:47:01 +0300

    Addded circle providers

---
+++ Hackgames/Assets/Scripts/CircleAnimateProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleAnimateProvider : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnRiseEnd()
+    {
+        animator.ResetTrigger("End");
+        //TODO: Circle stuff
+    }
+
+    public void OnCircleComplit()
+    {
+        animator.SetTrigger("End");
+    }
+
+    public void OnCircleEnd()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
