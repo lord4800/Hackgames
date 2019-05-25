ebf0000d69038c18604bc5f9dd62f61d1aa36583
reffrim <51008353+reffrim@users.noreply.github.com>
2019-05-25 22:40:24 +0300

    shevron

---
+++ Hackgames/Assets/ShevronActivator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShevronActivator : MonoBehaviour
+{
+    SpriteRenderer Renderer;
+    bool Enabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == this.gameObject.layer)
+        {
+            if (Renderer != null)
+                Renderer.enabled = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == this.gameObject.layer)
+        {
+            if (Renderer != null)
+                Renderer.enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ColliderEnter");
+
+    }
+}
