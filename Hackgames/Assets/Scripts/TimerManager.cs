e0348adeda11fe63c3a281ced628a6001e7bc620
lord4800 <killerontheroad4800@gmail.com>
2019-05-26 10:34:46 +0300

    [WIP] Added Timer

---
+++ Hackgames/Assets/Scripts/TimerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    private List<SpriteRenderer> timerPoints = new List<SpriteRenderer>();
+
+    private void Awake()
+    {
+        if (timerPoints == null || timerPoints.Count <= 0)
+        {
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
