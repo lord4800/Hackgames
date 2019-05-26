254df2a8c5ecdb8b54ca4298e05076db17628913
reffrim <51008353+reffrim@users.noreply.github.com>
2019-05-26 05:11:19 +0300

    Working with all circles. Adjusting Shevron srcipt.

---
+++ Hackgames/Assets/Scripts/AngleAdjuster.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleAdjuster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.up = Vector3.zero + transform.position;
+    }
+}
